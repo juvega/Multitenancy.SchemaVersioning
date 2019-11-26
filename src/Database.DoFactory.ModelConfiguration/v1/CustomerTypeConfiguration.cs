98fdefdaec8ddfd29c8812e31ecfd09d90102cd0
Cesar Velarde <cevegaju@hotmail.com>
2019-11-26 17:42:00 -0500

    Change folder structure

---
+++ src/Database.DoFactory.ModelConfiguration/v1/CustomerTypeConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+
+namespace Database.DoFactory.ModelConfiguration
+{
+    public class CustomerTypeConfiguration : IEntityTypeConfiguration<Models.Customer>
+    {
+        public void Configure(EntityTypeBuilder<Models.Customer> builder)
+        {
+            builder.ToTable("Customer");
+
+            builder.HasIndex(e => new { e.LastName, e.FirstName })
+                .HasName("IndexCustomerName");
+
+            builder.Property(e => e.City).HasMaxLength(40);
+
+            builder.Property(e => e.Country).HasMaxLength(40);
+
+            builder.Property(e => e.FirstName)
+                .IsRequired()
+                .HasMaxLength(40);
+
+            builder.Property(e => e.LastName)
+                .IsRequired()
+                .HasMaxLength(40);
+
+            builder.Property(e => e.Phone).HasMaxLength(20);
+        }
+    }
+}
---
+++ src/Database.DoFactory.ModelConfiguration/v1/OrderItemTypeConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using Database.DoFactory.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Database.DoFactory.ModelConfiguration
+{
+    public class OrderItemTypeConfiguration : IEntityTypeConfiguration<Models.OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.ToTable("OrderItem");
+
+            builder.HasIndex(e => e.OrderId)
+                .HasName("IndexOrderItemOrderId");
+
+            builder.HasIndex(e => e.ProductId)
+                .HasName("IndexOrderItemProductId");
+
+            builder.Property(e => e.Quantity).HasDefaultValueSql("((1))");
+
+            builder.Property(e => e.UnitPrice).HasColumnType("decimal(12, 2)");
+
+            builder.HasOne(d => d.Order)
+                .WithMany(p => p.OrderItem)
+                .HasForeignKey(d => d.OrderId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_ORDERITE_REFERENCE_ORDER");
+
+            builder.HasOne(d => d.Product)
+                .WithMany(p => p.OrderItem)
+                .HasForeignKey(d => d.ProductId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_ORDERITE_REFERENCE_PRODUCT");
+
+        }
+    }
+}
